4b9152656f393763060a423384aad61f40d5342c
Ben Critchley <Critchley_11@hotmail.co.uk>
2021-03-05 13:26:02 +0000

    End of section 18

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -17,6 +17,7 @@ namespace Application.Core
             .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
             .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
             CreateMap<AppUser, Profiles.Profile>().ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
+            CreateMap<Profiles.Profile, Profiles.Profile>();
         }
     }
 }
